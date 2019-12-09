631ed93fa18b90b21f5dc3467963ca57d242c087
dev001hajipro <dev001hajipro@gmail.com>
2019-12-10 06:03:04 +0900

    ボタンクリックでviewmodelが反応するところまで実装

--- app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/game/GameViewModel.kt
+++ app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.github.dev001hajipro.notorekraepelin.ui.game
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 
 class GameViewModel(application: Application) : AndroidViewModel(application) {
@@ -8,7 +9,8 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     fun onClickCancel() {
 
     }
-    fun onClickNumberPad() {
+    fun onClickNumberPad(number : Int) {
+        Log.d("DEBUG_X", "onClickNumberPad = $number")
 
     }
 }
