25570dcc4d7d7e665fe2b5701d22d1a3ced4ec92
dev001hajipro <dev001hajipro@gmail.com>
2019-12-16 21:51:38 +0900

    ゲーム結果画面も作成開始

--- app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/game/GameResultFragment.kt
+++ app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/game/GameResultFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.github.dev001hajipro.notorekraepelin.R
+import com.github.dev001hajipro.notorekraepelin.databinding.FragmentGameResultBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +19,10 @@ private const val ARG_PARAM2 = "param2"
  * １ゲーム終わったときの結果画面用フラグメント.
  */
 class GameResultFragment : Fragment() {
+
+    private val args: GameFragmentArgs by navArgs()
+    private val viewModel: GameResultViewModel by viewModels()
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,8 +39,11 @@ class GameResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_result, container, false)
+        val binding = FragmentGameResultBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+            it.viewModel = this.viewModel
+        }
+        return binding.root
     }
 
     companion object {
