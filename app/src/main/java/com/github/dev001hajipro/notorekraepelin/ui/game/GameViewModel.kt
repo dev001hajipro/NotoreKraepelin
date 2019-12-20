cc34e8091807877f2f5f82ad93dc653692447d61
dev001hajipro <dev001hajipro@gmail.com>
2019-12-21 06:25:44 +0900

    残り秒数をLiveData対応

--- app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/game/GameViewModel.kt
+++ app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/game/GameViewModel.kt
@@ -1,13 +1,9 @@
 package com.github.dev001hajipro.notorekraepelin.ui.game
 
 import android.app.Application
-import android.content.Intent
-import android.graphics.Color // TODO("delete ui code?")
 import android.os.Handler
 import android.util.Log
-import android.widget.Button // TODO("delete ui code")
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.github.dev001hajipro.notorekraepelin.Kraepelin
 import com.github.dev001hajipro.notorekraepelin.SingleLiveEvent
@@ -15,17 +11,24 @@ import com.github.dev001hajipro.notorekraepelin.SingleLiveEvent
 class GameViewModel(application: Application) : AndroidViewModel(application) {
 
     val handler = Handler()
-    var timerText = MutableLiveData(0)
+    var remainingSeconds = MutableLiveData(0)
 
     // UI側から変更なし
     var currentSecond = 0
     var cursorIndex = 0
+
+    var q1 = MutableLiveData(0)
+    var q2 = MutableLiveData(0)
+    var a1 = MutableLiveData(0)
+    var a2 = MutableLiveData(0)
+    var a3 = MutableLiveData(0)
+
     // UI側から変更なし
     var lines = MutableList(15) { Kraepelin.geneList115() }
     // UI側から変更なし
-    var scores  = MutableList(15) {0}
+    var scores = MutableList(15) { 0 }
     //
-    var misses = MutableList(15) {0}
+    var misses = MutableList(15) { 0 }
     // UI側から変更なし
     var lineCount = 0
 
@@ -38,7 +41,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         override fun run() {
             currentSecond++
             // TODO("need observe")
-            timerText.value = maxSecond.value?.minus(currentSecond) ?: 0
+            remainingSeconds.value = maxSecond.value?.minus(currentSecond) ?: 0
 
             if (currentSecond % 60 == 0) {
                 cursorIndex = 0
@@ -49,7 +52,6 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
                 if (currentSecond < it) {
                     handler.postDelayed(this, 1000)
                 } else {
-                    //TODO("goto the GameResultFragment.")
                     navigateToGameResultEvent.setValue(Any())
                 }
             }
@@ -65,11 +67,10 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         handler.removeCallbacks(runnable)
     }
 
-
-
     fun onClickCancel() {
 
     }
+
     fun start() {
         Log.d("onStart", "onStart")
 
@@ -77,48 +78,30 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         cursorIndex = 0
         lineCount = 0
 
-        // TODO("fragment argument")
-        //maxSecond = intent.getIntExtra("KEY_TIMER_SECONDS", 1)
+        remainingSeconds.value = maxSecond.value?.minus(currentSecond) ?: 0
 
-        // TODO("need binding")
-
-        timerText.value = maxSecond.value?.minus(currentSecond) ?: 0
-// TODO("need binding")
-        //textQ1.text = "${lines[lineCount][cursorIndex + 0]}"
-        //textQ2.text = "${lines[lineCount][cursorIndex + 1]}"
-// TODO("need binding")
-        //textA1.text = " "
-        //textA2.text = "_"
-        //textA3.text = " "
+        q1.value = lines[lineCount][cursorIndex + 0]
+        q2.value = lines[lineCount][cursorIndex + 1]
 
         // start timer.
         handler.postDelayed(runnable, 1000)
     }
-    fun onClickNumberPad(number : Int) {
+
+    fun onClickNumberPad(number: Int) {
         Log.d("DEBUG_X", "onClickNumberPad = $number")
-        // show input number
-        // TODO("delete ui code")
 
-        /*
-        val b = v as Button
-        var inputNumber = Integer.parseInt(b.tag.toString())
-        textA1.text = b.tag.toString()
-
-        val answer = (lines[lineCount][cursorIndex + 0] + lines[lineCount][cursorIndex + 1]) % 10
-
-        if (answer == inputNumber) {
-            textA1.setTextColor(Color.GREEN)
+        a1.value = number
+        val answer = lines[lineCount][cursorIndex + 0] + lines[lineCount][cursorIndex + 1] % 10
+        // todo("setTextColor")
+        if (answer == number) {
             scores[lineCount]++
         } else {
-            textA1.setTextColor(Color.RED)
             misses[lineCount]++
         }
-         */
 
-        // show next question.
+        // show data of the next question.
         cursorIndex++
-        // TODO("update ui")
-        //textQ1.text = "${lines[lineCount][cursorIndex + 0]}"
-        //textQ2.text = "${lines[lineCount][cursorIndex + 1]}"
+        q1.value = lines[lineCount][cursorIndex + 0]
+        q2.value = lines[lineCount][cursorIndex + 1]
     }
 }
