94b4b9d8aa5a9775a360ddbd9198cee61de63066
dev001hajipro <dev001hajipro@gmail.com>
2020-01-02 05:07:30 +0900

    ゲーム画面からオプションメニュー削除

--- app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/tools/ToolsFragment.kt
+++ app/src/main/java/com/github/dev001hajipro/notorekraepelin/ui/tools/ToolsFragment.kt
@@ -1,11 +1,12 @@
 package com.github.dev001hajipro.notorekraepelin.ui.tools
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.github.dev001hajipro.notorekraepelin.R
 import com.github.dev001hajipro.notorekraepelin.databinding.FragmentToolsBinding
 
 class ToolsFragment : Fragment() {
@@ -21,5 +22,22 @@ class ToolsFragment : Fragment() {
             it.lifecycleOwner = this
             it.viewModel = viewModel
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 }
